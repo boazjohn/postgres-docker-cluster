6ed351d8838d0d4915dbb2ddb5dd4d815a6f526f
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2017-01-12 14:13:17 +0530

    Separates build and up commands, adds some logs

--- tests/sync-failover.sh
+++ tests/sync-failover.sh
@@ -2,7 +2,10 @@ docker-machine rm default
 docker-machine create -d virtualbox --virtualbox-disk-size "30000" default
 docker-machine start default
 eval "$(docker-machine env default)"
-docker-compose up -d
+docker-compose build && docker-compose up -d
+echo "\n\n\n\n\n\n ############# SLEEPING NOW ############ \n\n\n\n\n\n"
+sleep 60
+
 docker-compose exec pgmaster bash -c "gosu postgres psql -c \"ALTER SYSTEM SET synchronous_standby_names TO 'node2,node3,node4,node5';\""
 docker-compose exec pgmaster bash -c "gosu postgres psql -c \"select pg_reload_conf();\""
 docker-compose exec pgmaster bash -c "gosu postgres psql -c \"SELECT application_name, sync_state FROM pg_stat_replication;\""
@@ -16,6 +19,7 @@ sleep 2
 docker-compose exec pgslave1 bash -c "gosu postgres psql -c \"SELECT pg_catalog.pg_last_xlog_receive_location();\""
 docker-compose exec pgslave3 bash -c "gosu postgres psql -c \"SELECT pg_catalog.pg_last_xlog_receive_location();\""
 
+echo "\n\n\n\n\n\n ############# KILLING MASTER NOW ############ \n\n\n\n\n\n"
 docker-compose kill pgmaster
 
 docker-compose logs -f
