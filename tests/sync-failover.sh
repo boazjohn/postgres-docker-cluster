7293d3507192983427a04fe74e43df2f28118c9c
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2017-01-12 20:58:00 +0530

    Makes the test run 5 times and log output in each run.

--- tests/sync-failover.sh
+++ tests/sync-failover.sh
@@ -1,25 +1,33 @@
-docker-machine rm default
-docker-machine create -d virtualbox --virtualbox-disk-size "30000" default
-docker-machine start default
-eval "$(docker-machine env default)"
-docker-compose build && docker-compose up -d
-echo "\n\n\n\n\n\n ############# SLEEPING NOW ############ \n\n\n\n\n\n"
-sleep 60
+#!/usr/bin/env bash
 
-docker-compose exec pgmaster bash -c "gosu postgres psql -c \"ALTER SYSTEM SET synchronous_standby_names TO 'node2,node3,node4,node5';\""
-docker-compose exec pgmaster bash -c "gosu postgres psql -c \"select pg_reload_conf();\""
-docker-compose exec pgmaster bash -c "gosu postgres psql -c \"SELECT application_name, sync_state FROM pg_stat_replication;\""
+for i in {1..5}
+do
+    docker-machine rm --force default
+    docker-machine create -d virtualbox --virtualbox-disk-size "30000" default
+    docker-machine start default
+    eval "$(docker-machine env default)"
+    docker-compose build && docker-compose up -d
+    echo "\n\n\n\n\n\n ############# SLEEPING NOW ############ \n\n\n\n\n\n"
+    sleep 60
 
-docker-compose exec pgslave3 bash -c "tc qdisc add dev eth0 root netem delay 200ms"
+    docker-compose exec pgmaster bash -c "gosu postgres psql -c \"ALTER SYSTEM SET synchronous_standby_names TO 'node2,node3,node4,node5';\""
+    docker-compose exec pgmaster bash -c "gosu postgres psql -c \"select pg_reload_conf();\""
+    docker-compose exec pgmaster bash -c "gosu postgres psql -c \"SELECT application_name, sync_state FROM pg_stat_replication;\""
 
-docker-compose exec pgmaster bash -c "gosu postgres pgbench -i -s 100" -d
+    docker-compose exec pgslave3 bash -c "tc qdisc add dev eth0 root netem delay 200ms"
 
-sleep 2
+    docker-compose exec pgmaster bash -c "gosu postgres pgbench -i -s 100" -T -d
 
-docker-compose exec pgslave1 bash -c "gosu postgres psql -c \"SELECT pg_catalog.pg_last_xlog_receive_location();\""
-docker-compose exec pgslave3 bash -c "gosu postgres psql -c \"SELECT pg_catalog.pg_last_xlog_receive_location();\""
+    docker-compose exec pgslave1 bash -c "gosu postgres psql -c \"SELECT pg_catalog.pg_last_xlog_receive_location();\""
+    docker-compose exec pgslave3 bash -c "gosu postgres psql -c \"SELECT pg_catalog.pg_last_xlog_receive_location();\""
 
-echo "\n\n\n\n\n\n ############# KILLING MASTER NOW ############ \n\n\n\n\n\n"
-docker-compose kill pgmaster
+    echo "\n\n\n\n\n\n ############# KILLING MASTER NOW ############ \n\n\n\n\n\n"
+    docker-compose kill pgmaster
 
-docker-compose logs -f
+    for i in {1..10}
+    do
+	docker-compose exec pgslave3 bash -c "gosu postgres repmgr cluster show" | grep "master" | cut -c 1-20 >> ./stdout.log
+	sleep 6
+    done
+    echo "test run complete" >> ./stdout.log
+done
